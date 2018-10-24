56e7e23ae862fa77d88e3c35cde26e5281743af4
AbeIgnocious <k.l.kubat@gmail.com>
2018-10-24 10:18:40 +0200

    add benchmarks

---
+++ reader_test.go
@@ -0,0 +1,70 @@
+package cellar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.etcd.io/bbolt"
+	"testing"
+	"time"
+)
+
+func TestReader_Scan(t *testing.T) {
+	db, err := New(dbDir, WithNoFileLock, WithMetaDB(newBoltMetaDB()))
+	require.NoError(t, err)
+
+	msg := "TestReader_Scan"
+	_, err = db.Append([]byte(msg))
+	require.NoError(t, err)
+
+	db.Flush()
+	reader := db.Reader()
+	passed := false
+	err = reader.Scan(func(pos *ReaderInfo, data []byte) error {
+		if string(data) == msg {
+			passed = true
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	assert.True(t, passed)
+}
+
+func TestRestartingDBWorks(t *testing.T) {
+
+	for i := 0; i < 100; i++ {
+		blt, err := bolt.Open(
+			"testdata/RestartingDBWorks.bolt",
+			0600,
+			&bolt.Options{Timeout: 1 * time.Second})
+
+		require.NoError(t, err)
+		b := &BoltMetaDB{DB: blt}
+		b.Init()
+		db, err := New(dbDir, WithNoFileLock, WithMetaDB(b))
+		require.NoError(t, err)
+
+		_, err = db.Append([]byte("RestartingDBWorks"))
+		require.NoError(t, err)
+		db.Flush()
+		db.Close()
+	}
+	blt, err := bolt.Open(
+		"testdata/RestartingDBWorks.bolt",
+		0600,
+		&bolt.Options{Timeout: 1 * time.Second})
+	db, err := New(dbDir, WithNoFileLock, WithMetaDB(&BoltMetaDB{DB: blt}))
+	require.NoError(t, err)
+
+	reader := db.Reader()
+	seen := 0
+	err = reader.Scan(func(pos *ReaderInfo, data []byte) error {
+		if string(data) == "RestartingDBWorks" {
+			seen++
+		}
+		return nil
+	})
+	require.NoError(t, err)
+
+	assert.True(t, seen == 100)
+
+}
