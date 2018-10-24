e9b86e97f46fcedfe12a964b7bd241a41dcc92f6
AbeIgnocious <k.l.kubat@gmail.com>
2018-10-24 13:06:27 +0200

    test ensureFolder

---
+++ util_test.go
@@ -0,0 +1,21 @@
+package cellar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_ensureFolder_folder_exists(t *testing.T) {
+	err := ensureFolder("testdata")
+	assert.NoError(t, err)
+}
+
+func Test_ensureFolder_folder_is_file(t *testing.T) {
+	err := ensureFolder("util.go")
+	assert.EqualError(t, err, ErrIsFile.Error())
+}
+
+func Test_ensureFolder_folder_not_exists(t *testing.T) {
+	err := ensureFolder("newfolder")
+	assert.NoError(t, err)
+}
