7928f637b2d93e0c85aa13eb200a58f85d323f38
AbeIgnocious <k.l.kubat@gmail.com>
2018-10-24 18:40:28 +0200

    create tests for options

---
+++ options_test.go
@@ -0,0 +1,48 @@
+package cellar
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestWithCipher(t *testing.T) {
+	aes := NewAES(defaultEncryptionKey)
+	db, err := New(dbDir, WithReadOnly, WithCipher(aes))
+	require.NoError(t, err)
+	assert.Equal(t, aes, db.cipher)
+}
+
+func TestWithLogger(t *testing.T) {
+	logger := defaultLogger()
+	db, err := New(dbDir, WithReadOnly, WithLogger(logger))
+	require.NoError(t, err)
+	assert.Equal(t, logger, db.logger)
+}
+
+func TestWithMetaDB(t *testing.T) {
+	mdb := &BoltMetaDB{}
+	db, err := New(dbDir, WithReadOnly, WithMetaDB(mdb))
+	require.NoError(t, err)
+	assert.Equal(t, mdb, db.meta)
+}
+
+func TestWithNoFileLock(t *testing.T) {
+	mdb := &BoltMetaDB{}
+
+	// WithMetaDB is provided because by default boltdb is used, which is
+	// rw once
+	_, err := New(dbDir, WithNoFileLock, WithReadOnly, WithMetaDB(mdb))
+	require.NoError(t, err)
+
+	_, err = New(dbDir, WithNoFileLock, WithReadOnly, WithMetaDB(mdb))
+	require.NoError(t, err)
+
+	// first db with file lock should work
+	_, err = New(dbDir, WithReadOnly, WithMetaDB(mdb))
+	require.NoError(t, err)
+
+	// second should fail because there now is a file lock
+	_, err = New(dbDir, WithReadOnly, WithMetaDB(mdb))
+	assert.Error(t, err)
+}
